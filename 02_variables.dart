f3d85953eb9322ee47910101061a10f00df88e70
doggbmx <manuelorticosta@gmail.com>
2023-08-09 10:59:49 -0400

    dynamic

--- 02_variables.dart
+++ 02_variables.dart
@@ -6,11 +6,22 @@ void main() {
   final List<String> abilities = ['impostor'];
   final sprites = <String>['ditto/front.png', 'ditto/back.png'];
 
+  // dynamic == null
+  // es lo mismo q usar el 'any' en typescript
+  dynamic errorMessage = 'Hola';
+  // acepta cualquier cosa
+  errorMessage = true;
+  errorMessage = [1, 2, 3, 4, 5];
+  errorMessage = {1, 2, 3, 4, 5};
+  errorMessage = () => true;
+  errorMessage = null;
+
   print("""
   $pokemon
   $hp
   $isAlive
   $abilities
   $sprites
+  $errorMessage
   """);
 }
