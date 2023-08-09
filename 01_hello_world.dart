729acdaa8a6dd102bd88b317cc95cf231863b491
doggbmx <manuelorticosta@gmail.com>
2023-08-09 10:45:31 -0400

    first commit

---
+++ 01_hello_world.dart
@@ -0,0 +1,11 @@
+// dart-pad exercise
+
+void main() {
+  // var myName = 'Manu';
+  // late final myName;
+  // String myName = 'Manu';
+  const myName = 'Manu';
+  print("hola $myName");
+  print("hola ${myName.toUpperCase()}");
+  print('hola ${1 + 1}');
+}
