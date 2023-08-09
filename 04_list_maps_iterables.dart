615fd58efb1fe2fc268ac1a2a9617db05d618801
doggbmx <manuelorticosta@gmail.com>
2023-08-09 11:28:28 -0400

    list-iterables-maps

---
+++ 04_list_maps_iterables.dart
@@ -0,0 +1,27 @@
+void main() {
+  final numbers = [1, 2, 3, 4, 5, 5, 5, 6, 7, 8, 8, 9, 10];
+
+  print('List original: $numbers');
+  print('List longitud: ${numbers.length}');
+  print('Index 0: ${numbers[0]}');
+  print('First: ${numbers.first}');
+  print('Last: ${numbers.last}');
+  // ESTO DEVUELVE UN ITERABLEEEE!!!
+  print('Reversed: ${numbers.reversed}');
+
+  final reversedNumbers = numbers.reversed;
+  print('Reversed: $reversedNumbers');
+  print('Lista: ${reversedNumbers.toList()}');
+  // un set no puede tener valores duplicados
+  print('Set: ${reversedNumbers.toSet()}');
+
+  // de esta manera podemos eliminar valores duplicados de una lista
+  print('List original: ${numbers.toSet().toList()}');
+
+  final numbersGreaterThan5 = numbers.where((num) {
+    return num > 5; // true
+  });
+
+  print('>5 iterables: $numbersGreaterThan5');
+  print('>5: ${numbersGreaterThan5.toSet()}');
+}
