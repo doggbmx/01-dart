fb5715083afaf64b12c2a84c075212ee53899aef
doggbmx <manuelorticosta@gmail.com>
2023-08-15 16:42:25 -0400

    extends

--- 09_abstract_class.dart
+++ 09_abstract_class.dart
@@ -1,4 +1,16 @@
-void main() {}
+void main() {
+  final windPlant = WindPlant(initialEnergy: 100);
+
+  print('wind: ${chargePhone(windPlant)}');
+}
+
+double chargePhone(EnergyPlant plant) {
+  if (plant.energyLeft < 10) {
+    throw Exception('Not enough energy left!');
+  }
+
+  return plant.energyLeft - 10;
+}
 
 enum PlantType {
   nuclear,
@@ -13,4 +25,16 @@ abstract class EnergyPlant {
   EnergyPlant({required this.energyLeft, required this.type});
 
   void consumeEnergy(double energy);
+}
+
+// extends o implements
+// extends hereda basicamente
+class WindPlant extends EnergyPlant {
+  WindPlant({required double initialEnergy})
+      : super(energyLeft: initialEnergy, type: PlantType.wind);
+
+  @override
+  void consumeEnergy(double energy) {
+    energyLeft -= energy;
+  }
 }
